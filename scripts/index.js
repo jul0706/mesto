365920e68a902e87738f9286a66043ec9f6c48d1
Yuliya Ivanovskaya <yu.ivan@rambler.ru>
2022-12-18 19:16:35 +0500

    добавить функцию отправки формы

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 //Объявляем переменные 
 const popupElement = document.querySelector('.popup');
+
 const popupCloseIconElement = popupElement.querySelector('.popup-container__close-icon');
 const editButtonElement = document.querySelector('.profile__edit-button');
 
@@ -14,13 +15,40 @@ const popupClose = function () {
     popupElement.classList.remove('popup_is-opened')
 }
 
-
-
 //назначаем кнопке редактирования обработчик события
 editButtonElement.addEventListener('click',popupOpen);
 
 // назначаем кнопке закрытия формы редактирования обработчик события
 popupCloseIconElement.addEventListener('click',popupClose);
 
+// Находим форму в DOM
+let formElement = document.querySelector('.form-edit-profile');
+// Находим поля формы в DOM
+let nameInput = formElement.querySelector('.form-edit-profile__input_name');
+let jobInput = formElement.querySelector('.form-edit-profile__input_job');
 
 
+
+let nameInputValue = nameInput.value;// Получите значение полей jobInput и nameInput из свойства value
+let jobInputValue = jobInput.value;
+    
+let profileTitleElement = document.querySelector('.profile__title');   // Выберите элементы, куда должны быть вставлены значения полей
+let profileSubtitleElement = document.querySelector('.profile__subtitle');
+
+
+   
+/* Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+function handleFormSubmit (event) {
+    event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    
+    // Вставьте новые значения с помощью textContent
+}
+
+/* Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
+
+*/
