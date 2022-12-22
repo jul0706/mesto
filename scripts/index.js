6085291699686fad3f7e9a9b9aa707bc28c4ddc1
Yuliya Ivanovskaya <yu.ivan@rambler.ru>
2022-12-22 10:16:04 +0500

    ревью1

--- scripts/index.js
+++ scripts/index.js
@@ -4,54 +4,50 @@ const popupCloseIconElement = popupElement.querySelector('.popup-container__clos
 const editButtonElement = document.querySelector('.profile__edit-button'); //находим кнопку "изменить"
 const formElement = document.querySelector('.form-edit-profile'); // Находим форму в DOM
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.form-edit-profile__input_type_name');
-let jobInput = formElement.querySelector('.form-edit-profile__input_type_job');
+const nameInput = formElement.querySelector('.form-edit-profile__input_type_name');
+const jobInput = formElement.querySelector('.form-edit-profile__input_type_job');
+// Находим элементы страницы, куда вставим значения полей формы
+const profileTitleElement = document.querySelector('.profile__title');   
+const profileSubtitleElement = document.querySelector('.profile__subtitle');
 
 //Функция открытия формы 
-const popupOpen = function () {
-    popupElement.classList.add('popup_is-opened')
+const openPopup = function () {
+    popupElement.classList.add('popup_is-opened');
+    nameInput.value = profileTitleElement.textContent;
+    jobInput.value = profileSubtitleElement.textContent;
 }
 
 //функция закрытия формы 
-const popupClose = function () {
+const closePopup = function () {
     popupElement.classList.remove('popup_is-opened')
 }
-
-//назначаем кнопке редактирования обработчик события
-editButtonElement.addEventListener('click', popupOpen);
-
-// назначаем кнопке закрытия формы обработчик события
-popupCloseIconElement.addEventListener('click', popupClose);
-
 
 // Обработчик «отправки» формы
 function handleFormSubmit (evt) {
 
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                
-    // Получаем значение полей jobInput и nameInput
-    let nameInputValue = nameInput.value;
-    let jobInputValue = jobInput.value;
-    
-    // Выбераем элементы, куда должны быть вставлены значения полей
-    let profileTitleElement = document.querySelector('.profile__title');   
-    let profileSubtitleElement = document.querySelector('.profile__subtitle');
 
     // Вставляем новые значения с помощью textContent
-    profileTitleElement.textContent = nameInputValue;  
-    profileSubtitleElement.textContent = jobInputValue;
+    profileTitleElement.textContent = nameInput.value;  
+    profileSubtitleElement.textContent = jobInput.value;
     
-    popupClose(); //закрываем форму
+    closePopup(); //закрываем форму
 };
 
+nameInput.addEventListener('click', function() { // добавляем обработчик очистки поля формы при клике
+    nameInput.value = '';
+    }
+);
 
+jobInput.addEventListener('click', function() { // добавляем обработчик очистки поля формы при клике
+    jobInput.value = '';
+    }
+);
 formElement.addEventListener('submit', handleFormSubmit); //добавлем обработчек событию отправки формы
 
-popupElement.addEventListener ('keydown', function (evt){ //добавляем обработчик на клавишу Enter
-        if (evt.code === 'Enter') {
-            formElement.submit();
-        }
-    })
+editButtonElement.addEventListener('click', openPopup); //назначаем кнопке редактирования обработчик события
+
+popupCloseIconElement.addEventListener('click', closePopup); // назначаем кнопке закрытия формы обработчик события
 
 
 
